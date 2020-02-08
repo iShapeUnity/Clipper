57e07535577a1819dae968bf4c61a0a4594b6c70
Nail Sharipov <nailxsharipov@gmail.com>
2020-02-08 18:33:16 +0300

    add tests

--- Tests/Editor/Tests/Clipper/BiteTests.cs
+++ Tests/Editor/Tests/Clipper/BiteTests.cs
@@ -1,5 +1,6 @@
 using iShape.Clipper.Solver;
 using iShape.Geometry;
+using iShape.Geometry.Container;
 using NUnit.Framework;
 using Tests.Clipper.Data;
 using Unity.Collections;
@@ -13,25 +14,129 @@ namespace Tests.Clipper {
 
         [Test]
         public void Test_00() {
-            var data = SubtractTestData.data[0];
+            var data = BiteTestData.data[0];
+            var solution = data.shape.Bite(data.path, iGeom, Allocator.Temp);
 
-            var master = new NativeArray<IntVector>(iGeom.Int(data[0]), Allocator.Temp);
-            var slave = new NativeArray<IntVector>(iGeom.Int(data[1]), Allocator.Temp);
+            Assert.AreEqual(solution.isInteract, true);
 
-            var solution = master.Intersect(slave, iGeom, Allocator.Temp);
+            Assert.AreEqual(solution.mainList.layouts.ToArray(),
+                new[] {
+                    new PathLayout(0, 4, true), 
+                    new PathLayout(4, 4, false)
+                });
 
-            Assert.AreEqual(solution.nature, SubtractSolution.Nature.overlap);
-            Assert.AreEqual(solution.pathList.Count, 1);
+            Assert.AreEqual(solution.mainList.points.ToArray(),
+                iGeom.Int(
+                    new[] {
+                        new Vector2(-15, -15),
+                        new Vector2(-15, 15),
+                        new Vector2(15, 15),
+                        new Vector2(15, -15),
+                        new Vector2(10, 10),
+                        new Vector2(10, -10),
+                        new Vector2(-10, -10),
+                        new Vector2(-10, 10)
+                })
+            );
 
-            var path = solution.pathList.Get(0, Allocator.Temp).ToArray();
-            var sample = iGeom.Int(new[] {
-                new Vector2(5, -10),
-                new Vector2(5, 0),
-                new Vector2(-5, 0),
-                new Vector2(-5, -10)
-            });
+            Assert.AreEqual(solution.biteList.layouts.ToArray(), new PathLayout[0]);
+            Assert.AreEqual(solution.biteList.points.ToArray(), new IntVector[0]);
 
-            Assert.AreEqual(path, sample);
+            solution.Dispose();
+        }
+        
+        [Test]
+        public void Test_01() {
+            var data = BiteTestData.data[1];
+            var solution = data.shape.Bite(data.path, iGeom, Allocator.Temp);
+
+            Assert.AreEqual(solution.isInteract, true);
+
+            Assert.AreEqual(solution.mainList.layouts.ToArray(),
+                new[] {
+                    new PathLayout(0, 4, true), 
+                    new PathLayout(4, 4, false)
+                });
+
+            Assert.AreEqual(solution.mainList.points.ToArray(),
+                iGeom.Int(
+                    new[] {
+                        new Vector2(-10, -10),
+                        new Vector2(-10, 10),
+                        new Vector2(10, 10),
+                        new Vector2(10, -10),
+                        new Vector2(-5, 5),
+                        new Vector2(-5, -5),
+                        new Vector2(5, -5),
+                        new Vector2(5, 5)
+                    })
+            );
+            
+            Assert.AreEqual(solution.biteList.layouts.ToArray(),
+                new[] {
+                    new PathLayout(0, 4, true)
+                });
+
+            Assert.AreEqual(solution.biteList.points.ToArray(),
+                iGeom.Int(
+                    new[] {
+                        new Vector2(-5, 5),
+                        new Vector2(-5, -5),
+                        new Vector2(5, -5),
+                        new Vector2(5, 5)
+                    })
+            );
+
+            solution.Dispose();
+        }
+        
+        [Test]
+        public void Test_02() {
+            var data = BiteTestData.data[2];
+            var solution = data.shape.Bite(data.path, iGeom, Allocator.Temp);
+
+            Assert.AreEqual(solution.isInteract, true);
+
+            Assert.AreEqual(solution.mainList.layouts.ToArray(),
+                new[] {
+                    new PathLayout(0, 4, true), 
+                    new PathLayout(4, 8, false)
+                });
+
+            Assert.AreEqual(solution.mainList.points.ToArray(),
+                iGeom.Int(
+                    new[] {
+                        new Vector2(-15, 15),
+                        new Vector2(15, 15),
+                        new Vector2(15, -15),
+                        new Vector2(-15, -15),
+                        new Vector2(-5, -5),
+                        new Vector2(-10, -5),
+                        new Vector2(-10, 10),
+                        new Vector2(5, 10),
+                        new Vector2(5, 5),
+                        new Vector2(10, 5),
+                        new Vector2(10, -10),
+                        new Vector2(-5, -10)
+                    })
+            );
+            
+            Assert.AreEqual(solution.biteList.layouts.ToArray(),
+                new[] {
+                    new PathLayout(0, 6, true)
+                });
+
+            Assert.AreEqual(solution.biteList.points.ToArray(),
+                iGeom.Int(
+                    new[] {
+                        new Vector2(-5, -5),
+                        new Vector2(5, -5),
+                        new Vector2(5, 5),
+                        new Vector2(10, 5),
+                        new Vector2(10, -10),
+                        new Vector2(-5, -10)
+                    })
+            );
 
             solution.Dispose();
         }
